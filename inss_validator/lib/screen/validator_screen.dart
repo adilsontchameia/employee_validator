2824da20a31ddf1de721f91c1fc2e1e1ee82ff2c
Adilson Tchameia <adilsonkchameia@outlook.com>
2023-02-01 14:31:08 +0100

    added image

--- inss_validator/lib/screen/validator_screen.dart
+++ inss_validator/lib/screen/validator_screen.dart
@@ -8,15 +8,20 @@ class QueryInformationScreen extends StatelessWidget {
     return Scaffold(
       body: Column(
         children: [
-          Container(
-            height: 300,
-            width: double.infinity,
-            decoration: const BoxDecoration(
-              color: Color.fromRGBO(14, 61, 97, 1),
-              borderRadius:
-                  BorderRadius.only(bottomLeft: Radius.circular(100.0)),
+          Stack(children: [
+            Container(
+                height: 300,
+                width: double.infinity,
+                decoration: const BoxDecoration(
+                    color: Color.fromRGBO(14, 61, 97, 1),
+                    borderRadius:
+                        BorderRadius.only(bottomLeft: Radius.circular(100.0)))),
+            Image.asset(
+              'assets/HaloLogoWhite.png',
+              height: MediaQuery.of(context).size.height * 0.40,
+              width: MediaQuery.of(context).size.width * 0.60,
             ),
-          ),
+          ]),
         ],
       ),
     );
