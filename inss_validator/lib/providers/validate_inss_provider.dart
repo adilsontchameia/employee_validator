6c98c19f867aee9c6714d1b825bb4c312c724ac1
Adilson Tchameia <adilsonkchameia@outlook.com>
2023-02-02 16:58:57 +0100

    created inss validator provider

---
+++ inss_validator/lib/providers/validate_inss_provider.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ValidateInssProvider extends ChangeNotifier {
+  final TextEditingController pin1 = TextEditingController();
+  final TextEditingController pin2 = TextEditingController();
+  final TextEditingController pin3 = TextEditingController();
+  final TextEditingController pin4 = TextEditingController();
+
+  //void validateInss(){}
+}
