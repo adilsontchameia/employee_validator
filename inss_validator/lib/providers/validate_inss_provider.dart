20dba7dbb772b43c00866d46bdf0e4d2ab788d9e
Adilson Tchameia <adilsonkchameia@outlook.com>
2023-02-07 14:26:02 +0100

    implemented inss provider

--- inss_validator/lib/providers/validate_inss_provider.dart
+++ inss_validator/lib/providers/validate_inss_provider.dart
@@ -1,16 +1,61 @@
 import 'package:flutter/material.dart';
+import 'package:inss_validator/api/google_sheets_api.dart';
+import 'package:inss_validator/data/staff_details.dart';
 
 class ValidateInssProvider extends ChangeNotifier {
-  final TextEditingController pin1 = TextEditingController();
-  final TextEditingController pin2 = TextEditingController();
-  final TextEditingController pin3 = TextEditingController();
-  final TextEditingController pin4 = TextEditingController();
-  int inss = 0000;
+  bool _isLoading = false;
+  StafDetails? staffDetails;
+  int? id;
+  String? name;
+  String? position;
+  String? code;
+  String? cashPaidStaff;
+  String? startDate;
+  String? birthDate;
+  String? idNumbers;
+  String? inssNum;
+  String? category;
+  String? gender;
+  String? bloodType;
+  String? location;
+  String? municipality;
+  String? dutyOcation;
+  String? teamSection;
+  String? opsWadSupport;
+  String? donor;
+  int? baseSalary = 0;
 
-  void validateInss(int pin1, int pin2, int pin3, int pin4) {
-    inss = pin1 + pin2 + pin3 + pin4;
-    //debugPrint('Insurance Number: $inss');
+  Future validateInss(int inss) async {
+    try {
+      isLoading = true;
+      final staff = await GoogleSheetApi.getById(inss);
+      name = staff!.name;
+      position = staff.position;
+      code = staff.code;
+      cashPaidStaff = staff.cashPaidStaff;
+      startDate = staff.startDate;
+      birthDate = staff.birthDate;
+      idNumbers = staff.idNumbers;
+      inssNum = staff.inssNum;
+      category = staff.category;
+      gender = staff.gender;
+      bloodType = staff.bloodType;
+      location = staff.location;
+      municipality = staff.municipality;
+      dutyOcation = staff.dutyOcation;
+      teamSection = staff.teamSection;
+      opsWadSupport = staff.opsWadSupport;
+      donor = staff.donor;
+      baseSalary = staff.baseSalary;
+    } finally {
+      isLoading = false;
+    }
+  }
 
+  bool get isLoading => _isLoading;
+
+  set isLoading(bool value) {
+    _isLoading = value;
     notifyListeners();
   }
 }
